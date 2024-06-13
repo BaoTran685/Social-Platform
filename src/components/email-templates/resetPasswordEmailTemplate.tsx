ff4df3c8d26d1e8f97674dcaa3a72f3933c8b4e8
ChrisPham03 <minhpham03032005@gmail.com>
2024-06-13 12:23:16 -0400

    email template

--- src/components/email-templates/resetPasswordEmailTemplate.tsx
+++ src/components/email-templates/resetPasswordEmailTemplate.tsx
@@ -10,41 +10,49 @@ export const ResetPasswordEmailTemplate: React.FC<Readonly<ResetPasswordEmailTem
    resetPasswordToken
 }) => (
   <center style={{ width: '100%', fontFamily: 'Helvetica, Arial, sans-serif', backgroundColor: '#ffffff', padding: '20px' }}>
-  <table align="center" width="100%" role="presentation" style={{maxWidth:'40em',padding:'20px',margin: '0 auto' ,backgroundColor:'#FFD133'}}>
-  <tr>
-          <td align="left" style={{ padding: '0', width: '100%', height: '10px', backgroundColor: '#FFD133',fontSize: '20px',fontWeight: 'bold' }}>
-              <p className="text-xs">
-              Chính m đã quên pass
-              </p>
-              </td>
-        </tr>
+    <table align="center" width="100%" role="presentation" style={{maxWidth:'40em',padding:'20px',margin: '0 auto' ,backgroundColor:'#FFD133'}}>
+      
+      <tr>
+        <td align="left" style={{ padding: '0', width: '100%', height: '10px', backgroundColor: '#FFD133',fontSize: '20px',fontWeight: 'bold' }}>
+          <p className="text-xs">
+            Chính m đã quên pass
+          </p>
+        </td>
+      </tr>
+      
       <table align="center" cellPadding="0" cellSpacing="0" width="100%" style={{backgroundColor:'#ffffff',margin: '0 auto'}}>
         <tbody>
-        <tr>
+          <tr>
             <td align="left" style={{padding: '0 15px', backgroundColor: '#efa600',color: '#000000', height: '10px', lineHeight: '30px' }}>
             </td>
           </tr>  
+          
           <tr>
             <td align="center" valign="top" style={{ backgroundColor: '#ffffff', padding: '20px 0' }}>
               <img src={'https://png.pngtree.com/png-vector/20230303/ourmid/pngtree-forgot-line-icon-vector-png-image_6628807.png'} width="70" height="70" style={{ border: '0' }} />
             </td>
           </tr>
+          
           <tr>
             <td align="center" style={{backgroundColor: '#ffffff', padding: '0 15px' }}>
               <span style={{color: '#000000', fontSize: '24px' }}>Forgot Your Password?</span>
             </td>
           </tr>
+
           <tr>
             <td style={{ height: '20px', lineHeight: '20px', fontSize: '20px' }}>&nbsp;</td>
           </tr>
+
           <tr>
             <td align="center" style={{ padding: '0 20px', fontSize: '20px' }}>
               <p>Click the button below to reset your password</p>
             </td>
           </tr>
+
           <tr>
             <td style={{ height: '20px', lineHeight: '20px', fontSize: '20px' }}>&nbsp;</td>
           </tr>
+
           <tr>
             <td align="center">
               <table cellPadding="0" cellSpacing="0" style={{ minWidth: '80%' }}>
@@ -58,26 +66,30 @@ export const ResetPasswordEmailTemplate: React.FC<Readonly<ResetPasswordEmailTem
               </table>
             </td>
           </tr>
+
           <td align="center">
             <div  style={{ margin: '20px 0', borderTop: '1px solid #eaeaea', width: '90%', borderWidth: '1px 0 0 0', borderColor: '#eaeaea', borderStyle: 'solid', }}></div>
           </td>
+
           <tr>
             <td align="left" style={{padding: '0 15px', backgroundColor: '#efa600',color: '#000000', height: '15px', lineHeight: '30px' }}>
-            <p className="text-xs">
-                Contact Bao Chan Team For Further Assist.
-            </p>
+              <p className="text-xs">
+                  Contact Bao Chan Team For Further Assist.
+              </p>
             </td>
           </tr>  
+
           <tr>
-          <td align="center" style={{ padding: '0 20px', width: '100%', height: '50px', backgroundColor: '#FFD133' }}>
+            <td align="center" style={{ padding: '0 20px', width: '100%', height: '50px', backgroundColor: '#FFD133' }}>
               <p className="text-xs">
               © Copyright Lmao Lmao.
               </p>
             </td>
-        </tr>
+          </tr>
+          
         </tbody>
-       </table>
       </table>
-    </center>
+    </table>
+  </center>
 );
 
