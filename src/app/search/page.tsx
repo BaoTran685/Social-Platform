1cb9b37bfe28864e053ec428c841bf5bc3236062
ChrisPham03 <minhpham03032005@gmail.com>
2024-06-13 12:19:03 -0400

    email template

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,8 +2,13 @@
 
 const SearchPage = () => {
   return (
-    <>
-    </>
+    <div className="flex justify-center items-center">
+    <input className="w-1/2 max-w-md p-2 bg-[#e7e7e76b] rounded-full shadow-md text-lg placeholder-gray-500 focus:outline-none" placeholder="Search...">
+    </input>
+  </div>
+
+
+  
   )
 }
 
--- src/components/email-templates/verifyEmailEmailTemplate.tsx
+++ src/components/email-templates/verifyEmailEmailTemplate.tsx
@@ -2,22 +2,93 @@
 
 import * as React from 'react';
 
+
 interface VerifyEmailEmailTemplateProps {
-  email: string,
   verifyEmailToken: string,
 }
 
 export const VerifyEmailEmailTemplate: React.FC<Readonly<VerifyEmailEmailTemplateProps>> = ({
-  email, verifyEmailToken,
+  verifyEmailToken,
 }) => (
-  <div>
-    <h1>Verify for <b>{email}</b></h1>
-    <p>
-      To verify,
-    </p>
-    <a href={`http://localhost:3000/auth/verify-email?token=${verifyEmailToken}`}>
-      Click here to verify email
-    </a>
-  </div>
-);
+  <center style={{ width: '100%', fontFamily: 'Helvetica, Arial, sans-serif', backgroundColor: '#ffffff', padding: '20px' }}>
+    <table align="center" width="100%" role="presentation" style={{maxWidth:'40em',padding:'20px',margin: '0 auto' ,backgroundColor:'#FFD133'}}>
+      <tr>
+        <td align="left" style={{ padding: '0', width: '100%', height: '10px', backgroundColor: '#FFD133',fontSize: '20px',fontWeight: 'bold' }}>
+            <p className="text-xs">
+              Chính m đã gửi mail
+            </p>
+          </td>
+      </tr>
+        <table align="center" cellPadding="0" cellSpacing="0" width="100%" style={{backgroundColor:'#ffffff',margin: '0 auto'}}>
+          <tbody>
+            <tr>
+              <td align="left" style={{padding: '0 15px', backgroundColor: '#efa600',color: '#000000', height: '10px', lineHeight: '30px' }}>
+              </td>
+            </tr>  
+            
+            <tr>
+              <td align="center" valign="top" style={{ backgroundColor: '#ffffff', padding: '20px 0' }}>
+                <img src={'https://static.thenounproject.com/png/98018-200.png'} width="70" height="70" style={{ border: '0' }} />
+              </td>
+            </tr>
+            
+            <tr>
+              <td align="center" style={{backgroundColor: '#ffffff', padding: '0 15px' }}>
+                <span style={{color: '#000000', fontSize: '24px' }}>Email Verification</span>
+              </td>
+            </tr>
 
+            <tr>
+              <td style={{ height: '20px', lineHeight: '20px', fontSize: '20px' }}>&nbsp;</td>
+            </tr>
+
+            <tr>
+              <td align="left" style={{ padding: '0 20px', fontSize: '15px' }}>
+                <p>Hi ....!</p>
+                <p>Thanks for starting the email sign-up process. We want to make sure it's really you. Please click the button below to verify. If you don't want to use email, you can ignore this message.</p>
+              </td>
+            </tr>
+
+            <tr>
+              <td style={{ height: '20px', lineHeight: '20px', fontSize: '20px' }}>&nbsp;</td>
+            </tr>
+
+            <tr>
+              <td align="center">
+                <table cellPadding="0" cellSpacing="0" style={{ minWidth: '80%' }}>
+                  <tbody>
+                    <tr>
+                      <td align="center" style={{ backgroundColor: '#efa600', padding: '6px', border: '1px solid #000000' }}>
+                        <a href={`http://localhost:3000/auth/verify-email?token=${verifyEmailToken}`} style={{ fontSize: '15px', fontWeight: 'bold', color: '#000000', textDecoration: 'none' }}>Verify My Email Address</a>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </td>
+            </tr>
+            
+            <td align="center">
+              <div  style={{ margin: '20px 0', borderTop: '1px solid #eaeaea', width: '90%', borderWidth: '1px 0 0 0', borderColor: '#eaeaea', borderStyle: 'solid', }}></div>
+            </td>
+            
+            <tr>
+              <td align="left" style={{padding: '0 15px', backgroundColor: '#efa600',color: '#000000', height: '15px', lineHeight: '30px' }}>
+                <p className="text-xs">
+                  From Bao Chan Team with luv
+                </p>
+              </td>
+            </tr>  
+
+            <tr>
+               <td align="center" style={{ padding: '0 20px', width: '100%', height: '50px', backgroundColor: '#FFD133' }}>
+                  <p className="text-xs">
+                  © Copyright Lmao Lmao.
+                  </p>
+               </td>
+            </tr>
+
+          </tbody>
+        </table>
+      </table>
+   </center>
+  );
